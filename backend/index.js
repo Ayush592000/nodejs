0a43800319f2e692f1afd9b334cf793d8291223e
Ayush592000 <ayushmalhotra020@gmail.com>
2024-12-17 13:09:13 +0530

    jwt token added

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
-
-app.listen(PORT,()=>{
+app.post('/login', (req, res) => {
+  const user = {
+    id: 1,
+    name: 'Anku',
+    email: 'anku12@gmali.com'
+  }
+  jwt.sign({ user }, process.env.SECRET_KEY, { expiresIn: '300s' }, (err, token) => {
+    res.json({
+      token
+    })
+  })
+})
+app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`)
 })
